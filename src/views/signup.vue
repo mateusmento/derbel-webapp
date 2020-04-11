e2ee0cee1412a4587e4a2a399ec8711d0ee81088
Mateus Sarmento <omateusmento@gmail.com>
2020-04-11 19:06:45 -0300

    Add signup

---
+++ src/views/signup.vue
@@ -0,0 +1,16 @@
+<template>
+	<section class="signin">
+		<header>
+			<h1>Derbel</h1>
+			<h3>Sign up a new account</h3>
+		</header>
+
+		<form>
+			<input type="text" placeholder="Full Name" />
+			<input type="text" placeholder="Username" />
+			<input type="password" placeholder="Password" />
+			<button type="submit">Sign Up</button>
+			<router-link to="/signin">Sign in with your account</router-link>
+		</form>
+	</section>
+</template>
