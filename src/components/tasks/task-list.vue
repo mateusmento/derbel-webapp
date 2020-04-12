8d682f7c256607721a4c781c7638708b0e970567
Mateus Sarmento <omateusmento@gmail.com>
2020-04-12 10:08:27 -0300

    Add task-list component

---
+++ src/components/tasks/task-list.vue
@@ -0,0 +1,18 @@
+<template>
+	<section class="task-list">
+		<div v-for="task of tasks" :key="task.id" class="task">
+			<button>Completed</button>
+			<span>{{ task.title }}</span>
+		</div>
+		<div class="new-task">
+			<button>Add</button>
+			<input type="text" />
+		</div>
+	</section>
+</template>
+
+<script lang="ts">
+export default {
+	props: ["tasks"]
+};
+</script>
