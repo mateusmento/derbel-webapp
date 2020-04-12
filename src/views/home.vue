2a385e988b4684fbae87061c454028cdee67aaf8
Mateus Sarmento <omateusmento@gmail.com>
2020-04-12 10:08:42 -0300

    Add home component

---
+++ src/views/home.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="home">
+		<header>
+			<navbar />
+			<user-menu />
+		</header>
+		<task-details />
+		<main>
+			<task-list id="todo" :tasks="tasks.todo" />
+			<task-list id="doing" :tasks="tasks.doing" />
+			<task-list id="done" :tasks="tasks.done" />
+		</main>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Tasks } from "@/model/tasks.collection";
+
+@Component({})
+export default class HomeComponent extends Vue {
+	private tasks(): Tasks {
+		return this.$store.getters.tasks;
+	}
+}
+</script>
