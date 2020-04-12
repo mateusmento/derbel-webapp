5e23c0239ed4249aced6d45933fa12bfbcde169e
Mateus Sarmento <omateusmento@gmail.com>
2020-04-12 10:03:51 -0300

    Add tasks, selectedTask and user state data

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,28 @@
+import { Stage } from '@/model/stage.enum';
+import { Task } from '@/model/task.model';
+import { Tasks } from '@/model/Tasks.collection';
+import { User } from '@/model/user.model';
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const user = new User(1, 'User');
+
 export default new Vuex.Store({
-	state: {},
+	state: {
+		user,
+		selectedTask: undefined,
+		tasks: [
+			new Task("Task 1", user, Stage.DONE),
+			new Task("Task 2", user, Stage.DONE),
+			new Task("Task 3", user, Stage.DOING),
+			new Task("Task 4", user)
+		],
+	},
+	getters:{
+		tasks: state => new Tasks(...state.tasks),
+	},
 	mutations: {},
 	actions: {},
 	modules: {}
