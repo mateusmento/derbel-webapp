9af967a36c44024c01520bd5bb95c28ce33447f8
Mateus Sarmento <omateusmento@gmail.com>
2020-04-12 10:03:22 -0300

    Add Tasks collection

---
+++ src/model/tasks.collection.ts
@@ -0,0 +1,20 @@
+import { Stage } from './stage.enum';
+import { Task } from './task.model';
+
+export class Tasks extends Array<Task> {
+	public constructor(...items: Task[]) {
+		super(...items);
+	}
+
+	public get todo() {
+		return this.filter(t => t.stage === Stage.TODO)
+	}
+
+	public get doing() {
+		return this.filter(t => t.stage === Stage.DOING)
+	}
+
+	public get done() {
+		return this.filter(t => t.stage === Stage.DONE)
+	}
+}
