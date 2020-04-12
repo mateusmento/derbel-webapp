5f4b748446d148ab83bbcbe759dded2919e4e1a9
Mateus Sarmento <omateusmento@gmail.com>
2020-04-12 10:05:07 -0300

    Add user-menu component

---
+++ src/components/shared/user-menu.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="user-menu">
+		<img :src="user.profile.photo" />
+		<span>{{ user.profile.name }}</span>
+		<i>More Options</i>
+		<ul class="menu">
+			<router-link>Settings</router-link>
+			<router-link>Sign out</router-link>
+		</ul>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { User } from "../../model/user.model";
+
+@Component({})
+export default class UserMenuComponent extends Vue {
+	private user: User = this.$store.state.user;
+}
+</script>
