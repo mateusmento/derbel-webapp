eae4ceb3c21a0147fc76dd2f394541e85fd31ff0
Mateus Sarmento <omateusmento@gmail.com>
2020-04-12 10:04:04 -0300

    Add navbar component

---
+++ src/components/shared/navbar.vue
@@ -0,0 +1,8 @@
+<template>
+	<nav>
+		<router-link>Tasks</router-link>
+		<router-link>Calendar</router-link>
+		<router-link>Notebooks</router-link>
+		<router-link>Rewards</router-link>
+	</nav>
+</template>
