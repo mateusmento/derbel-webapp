61e54b1e0e07d59526f0be9396e4a385a5d33046
Mateus Sarmento <omateusmento@gmail.com>
2020-04-12 10:08:05 -0300

    Add task-details component

---
+++ src/components/tasks/task-details.vue
@@ -0,0 +1,31 @@
+<template>
+	<aside v-if="!task">
+		<h1>Task Title</h1>
+		<ul class="steps">
+			<li v-for="step of task.steps" :key="step.id" class="step">
+				<span>{{ step.title }}</span>
+				<button>Completed</button>
+			</li>
+			<li class="new-step">
+				<button>Add</button>
+				<input type="text" />
+			</li>
+		</ul>
+		<label>
+			Description
+			<textarea cols="30" rows="10"></textarea>
+		</label>
+	</aside>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Task } from "@/model/task.model";
+
+@Component({})
+export default class TaskDetailsComponent extends Vue {
+	private get task(): Task | null {
+		return this.$store.state.selectedTask;
+	}
+}
+</script>
